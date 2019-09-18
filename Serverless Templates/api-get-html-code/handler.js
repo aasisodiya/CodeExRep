87919a162718ec13c568a241741f48f03ef6a3b9
Akash Sisodiya <akash_sisodiya@persistent.com>
2019-09-18 17:41:24 +0530

    Serverless Template : Get HTML - Incomplete
    Serverless Template : Get HTML - Incomplete, couldn't remove the quotes and also apply settings shown in link https://softwarebrothers.co/blog/render-one-file-html-pages-with-aws-api-gateway-lambda-using-nodejs/

---
+++ Serverless Templates/api-get-html-code/handler.js
@@ -0,0 +1,6 @@
+// Author : Akash Sisodiya
+'use strict';
+
+module.exports.hello = async event => {
+  return '<h1>Hello</h1>'; //not sure what's happening here but this seems to work but there are quotes in final o/p
+};
